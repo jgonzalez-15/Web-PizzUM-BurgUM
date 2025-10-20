08e665c5b5ffaffc7ffbbc67eaee994d306f7b72
Joacuu1675 <joaquingipes@gmail.com>
2025-10-20 09:13:34 -0300

    ViewOrders
    quedo hecha la pagina vieworders

---
+++ frontend/src/Components/OrderStatus.jsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom"
+
+export default function OrderStatus({id, date, status}){
+    return(
+    <>
+        <div className="w-full flex flex-row justify-between items-center bg-gray-50 rounded-2xl shadow-xl
+        h-32">
+            <div className="m-4 flex flex-col">
+                <h1 className="font-bold text-xl">Pedido #{id}</h1>
+                <h2>Fecha:{date}</h2>
+            </div>
+            <div className="m-4 flex flex-row gap-8 items-center">
+                <div className="flex flex-col items-center">
+                    <div className={`h-12 w-12 rounded-full ${status >= 0 ? "bg-orange-500" : "bg-gray-300"}`}/>
+                    <h1 className="text-center">Pedido</h1>
+                </div>
+                <div className="flex flex-col items-center">
+                    <div className={`h-12 w-12 rounded-full ${status >= 1 ? "bg-orange-500" : "bg-gray-300"}`}/>
+                    <h1 className="text-center">Preparado</h1>
+                </div>
+                <div className="flex flex-col items-center">
+                    <div className={`h-12 w-12 rounded-full ${status >= 2 ? "bg-orange-500" : "bg-gray-300"}`}/>
+                    <h1 className="text-center">Enviado</h1>
+                </div>
+                <div className="flex flex-col items-center">
+                    <div className={`h-12 w-12 rounded-full ${status >= 3 ? "bg-orange-500" : "bg-gray-300"}`}/>
+                    <h1 className="text-center">Recibido</h1>
+                </div>
+                {status == 0 && (
+                <button className={`z-0 h-12 transition-transform duration-100 ease-in-out hover:scale-102 rounded-2xl shadow-2xl font-bold m-1 text-sm md:text-base 2xl:text-xl text-center bg-red-600 text-white`}>
+                    <h2 className="m-2 2xl:m-3">
+                        Cancelar
+                    </h2>
+                </button>)}
+                {status != 0 && (
+                <button className={`z-0 h-12 rounded-2xl shadow-2xl font-bold m-1 text-sm md:text-base 2xl:text-xl text-center bg-gray-300`}>
+                    <h2 className="m-2 2xl:m-3">
+                        Cancelar
+                    </h2>
+                </button>)}
+            </div>
+        </div>     
+    </>
+    )
+}
--- frontend/src/Components/SmallButton.jsx
+++ frontend/src/Components/SmallButton.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 function SmallButton({text, isPrimary, route}){
     return(
-        <Link to={route} className={`z-0 transition-transform duration-100 ease-in-out hover:scale-102 rounded-2xl shadow-2xl font-bold m-1 text-sm md:text-base 2xl:text-xl text-center ${isPrimary ? "bg-orange-400 text-white" : "bg-gray-300 text-black"}`}>
+        <Link to={route} className={`z-0 transition-transform duration-100 ease-in-out hover:scale-102 rounded-2xl shadow-2xl font-bold m-1 text-sm md:text-base 2xl:text-xl text-center max-w-64 ${isPrimary ? "bg-orange-400 text-white" : "bg-gray-300 text-black"}`}>
             <h2 className="m-2 2xl:m-3">
                 {text}
             </h2>
