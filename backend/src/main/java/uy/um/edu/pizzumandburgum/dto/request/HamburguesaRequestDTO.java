030e7417806e11a051e653a2cac04dcb23f30ee6
Agustín Caetano <agus.caetano@gmail.com>
2025-10-31 09:04:12 -0300

    requestDTO limpios

--- backend/src/main/java/uy/um/edu/pizzumandburgum/dto/request/HamburguesaRequestDTO.java
+++ backend/src/main/java/uy/um/edu/pizzumandburgum/dto/request/HamburguesaRequestDTO.java
@@ -1,11 +1,9 @@
 package uy.um.edu.pizzumandburgum.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import uy.um.edu.pizzumandburgum.entities.HamburguesaProducto;
 
 import java.util.List;
 
--- backend/src/main/java/uy/um/edu/pizzumandburgum/dto/request/PedidoRequestDTO.java
+++ backend/src/main/java/uy/um/edu/pizzumandburgum/dto/request/PedidoRequestDTO.java
@@ -4,15 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.cglib.core.Local;
-import uy.um.edu.pizzumandburgum.dto.response.ClienteResponseDTO;
-import uy.um.edu.pizzumandburgum.dto.response.MedioDePagoDTO;
 import uy.um.edu.pizzumandburgum.dto.response.PedidoBebidaResponseDTO;
 import uy.um.edu.pizzumandburgum.dto.response.PedidoCreacionDTO;
-import uy.um.edu.pizzumandburgum.entities.*;
 
-import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 @Getter
@@ -20,11 +14,9 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PedidoRequestDTO {
-
         private String idCliente;
         private Long idDomicilio;
         private Long idMedioDePago;
-
         private List<PedidoCreacionDTO> creaciones;
         private List<PedidoBebidaResponseDTO> bebidas;
 }
