20f553d26bf019b2b950fd6da12e3881c6899908
Santi805312 <sonandi1503@gmail.com>
2025-11-03 09:54:27 -0300

    Se agregan notificaciones.

---
+++ backend/src/main/java/uy/um/edu/pizzumandburgum/entities/HistoricoProductoModificaciones.java
@@ -0,0 +1,16 @@
+package uy.um.edu.pizzumandburgum.entities;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class HistoricoProducto {
+    
+}
