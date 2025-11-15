c1e5147279a2f1910c735edc05c8f8fab2071686
jgonzalez-15 <joaquingipes@gmail.com>
2025-11-15 15:40:03 -0300

    eliminados datos de acceso a la base de datos

--- backend/src/main/java/uy/um/edu/pizzumandburgum/PizzUMBurgUM.java
+++ backend/src/main/java/uy/um/edu/pizzumandburgum/PizzUMBurgUM.java
@@ -8,12 +8,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class PizzUMBurgUM {
 
 	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
-
         SpringApplication.run(PizzUMBurgUM.class, args);
 	}
 
